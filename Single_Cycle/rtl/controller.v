5e784f7e1d4a8a61e8d83f2b59a4d3d64453c051
Harsha2635 <yamasaniprasanna2635@gmail.com>
2026-05-22 23:04:08 +0530

    Update controller.v

--- Single_Cycle/rtl/controller.v
+++ Single_Cycle/rtl/controller.v
@@ -20,9 +20,9 @@ module controller(
     //Branch_resultSrc_memWrite_aluSrc_immSrc_regWrite_aluOp_jalr
     always @(*) begin
         casez(opcode)
-            7'b0000011 : controls = 11'b0_01_0_1_00_0_00_0; //ld, etc.
+            7'b0000011 : controls = 11'b0_01_0_1_00_1_00_0; //ld, etc.
             7'b0010011 : controls = 11'b0_00_0_1_00_1_10_0; //addi, etc.
-            7'b0100011 : controls = 11'b0_01_1_1_01_1_00_0; //sd, etc.
+            7'b0100011 : controls = 11'b0_01_1_1_01_0_00_0; //sd, etc.
             7'b0110011 : controls = 11'b0_00_0_0_xx_1_10_0; //add, mul, div, rem etc.
             7'b1100011 : controls = 11'b1_xx_0_0_10_0_01_0; //beq, etc.
             7'b0?10111 : controls = 11'b0_11_0_x_xx_1_xx_0; //lui, auipc
